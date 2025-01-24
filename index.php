91dd849264270b172f637f017f57921960bad1c8
Senkaizen <97869023+HeroJago@users.noreply.github.com>
2025-01-24 23:40:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['quote'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta property="og:title" content=<?php "$quote" ?> />
+    <meta property="og:title" content=<?php $quote ?> />
     <meta property="og:type" content="product" />
     <meta property="og:description" content="*Quotes Hari Ini*" />
     <meta property="og:image:type" content="image/png" />
