ff8738f40d128ef1bea3781b11a476c957ee0dfd
Senkaizen <97869023+HeroJago@users.noreply.github.com>
2025-01-25 00:14:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,27 +9,29 @@ function codeexcpref($data) {
     return $data;
 }
 
-$quote = codeexcpref($_GET["quote"]);
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['quote'])) {
+    $quote = codeexcpref($_GET["quote"]);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta property="og:title" content=<?php $quote ?> />
+    <meta property="og:title" content=<?php "$quote" ?> />
     <meta property="og:type" content="product" />
     <meta property="og:description" content="*Quotes Hari Ini*" />
     <meta property="og:image:type" content="image/png" />
     <meta property="og:image:type" content="wangsaf.PNG" />
     <meta property="og:image" content="wangsaf.PNG" />
-    <meta property="og:image:width" content="1920" />
-    <meta property="og:image:height" content="1080" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
     <title><?= $spr ?></title>
 </head>
 <body>
     <h1><?= $spr ?></h1>
 
     <h3><?= $quote ?></h3>
-    <img src="wangsaf.PNG" alt="Quote Logo">
+    <img src="wangsaf.PNG" alt="Foto Duit">
 </body>
 </html>
